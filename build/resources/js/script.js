b903d7dd9d368b793ca8d2777dfcf7f9fd698c48
howardwarren1982 <howardwarren89@gmail.com>
2022-05-16 17:22:16 -0400

    added animations, working on main section and footer

--- build/resources/js/script.js
+++ build/resources/js/script.js
@@ -1,31 +1,44 @@
-// Demo Code
-// --------------------------------------------------------
+const set_obsever = function (element_by_id, obervered) {
+  const bar = document.getElementById(element_by_id);
+  bar.id = "none";
 
-// const controlsLink = document.querySelector(".controls");
-// controlsLink.addEventListener("click", function () {
-//   document.querySelector(".pic-rail").style.animationName = "none";
-// });
+  //Create the observer, same as before:
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        //chacge id
+        bar.id = element_by_id;
+      }
+      return;
+    });
+  });
 
-// const hoverFontChange = function (hoverElement, changingElement) {
-//   hoverElement.addEventListener("mouseover", function () {
-//     changingElement.style.fontSize = "45px";
-//   });
+  observer.observe(document.querySelector(obervered));
+};
 
-//   hoverElement.addEventListener("mouseout", function () {
-//     changingElement.style.fontSize = "34px";
-//   });
-// };
+const redbar_array = ["redBar1", "redBar2", "redBar3", "redBar4", "redBar5"];
+const bluebar_array = [
+  "blueBar1",
+  "blueBar2",
+  "blueBar3",
+  "blueBar4",
+  "blueBar5",
+];
+const polygons_array = ["BluePolygon", "RedPolygon"];
+const calendar_dates_array = ["eight", "nine", "ten", "eighteen"];
 
-// const basicButton = document.querySelector(".basic-order");
-// const planColOne = document.querySelector(".js-col-1");
-// hoverFontChange(basicButton, planColOne);
+redbar_array.forEach((element) => {
+  set_obsever(element, ".observepoint2");
+});
 
-// const standardButton = document.querySelector(".standard-order");
-// const planColTwo = document.querySelector(".js-col-2");
-// hoverFontChange(standardButton, planColTwo);
+bluebar_array.forEach((element) => {
+  set_obsever(element, ".observepoint2");
+});
 
-// const premiumButton = document.querySelector(".premium-order");
-// const planColThree = document.querySelector(".js-col-3");
-// hoverFontChange(premiumButton, planColThree);
+polygons_array.forEach((element) => {
+  set_obsever(element, ".observepoint1");
+});
 
-// --------------------------------------------------------
+calendar_dates_array.forEach((element) => {
+  set_obsever(element, ".observepoint3");
+});
